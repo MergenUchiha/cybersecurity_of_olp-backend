69a31499e0136cfed9993a084c957a76657ed452
MergenUchiha <mergendanat11@gmail.com>
2026-03-15 20:00:41 +0500

    fix(app): main ts fixed

--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 6000;
   await app.listen(port, '0.0.0.0');
   console.log(`🚀 Server running on http://0.0.0.0:${port}`);
   console.log(`📚 Swagger docs: http://localhost:${port}/api/docs`);
